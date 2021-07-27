e0dbd96965a853ba830ceb9b5f98fcb56256bb42
mohammedalsamki <momosamak21@gmail.com>
2021-07-27 16:19:29 +0300

    conver from var to git

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 'use strict';
 
 function nameOfCustomer(){
-  var customerName = prompt('What is your name?');
+  let customerName = prompt('What is your name?');
   alert('Hello ' + customerName + ' nice to meet you! Welcome to my site.');
 }
 nameOfCustomer();
 
-var correct = 0;
+let correct = 0;
 
 function question1(){
-  var childhood = prompt('Did I grow up in Seattle?');
+  let childhood = prompt('Did I grow up in Seattle?');
   if(childhood.toLowerCase() === 'no'){
     alert('You are correct');
     correct++;
@@ -20,7 +20,7 @@ function question1(){
 question1();
 
 function question2(){
-  var job = prompt('Have I worked in tech before?');
+  let job = prompt('Have I worked in tech before?');
   if(job.toUpperCase() === 'NO'){
     alert('You are correct');
     correct++;
@@ -32,7 +32,7 @@ question2();
 
 
 function question3(){
-  var age = prompt('Am I 26 years of age?');
+  let age = prompt('Am I 26 years of age?');
   if(age.toLowerCase() === 'yes'){
     alert('You are correct');
     correct++;
@@ -44,7 +44,7 @@ question3();
 
 
 function question4(){
-  var school = prompt('Did I attend College?');
+  let school = prompt('Did I attend College?');
   if(school.toLowerCase() === 'yes'){
     alert('You are correct');
     correct++;
@@ -57,7 +57,7 @@ question4();
 
 
 function question5(){
-  var employer = prompt('Is my ideal employer Lockheed?');
+  let employer = prompt('Is my ideal employer Lockheed?');
   if(employer.toUpperCase() === 'YES'){
     alert('You are correct');
     correct++;
@@ -69,9 +69,9 @@ question5();
 
 
 function question6(){
-  var guess = prompt('Guess a number between 1 and 10');
-  var numGuess = Number(guess);
-  var answer = 8;
+  let guess = prompt('Guess a number between 1 and 10');
+  let numGuess = Number(guess);
+  let answer = 8;
 
   for (var i = 0; i < 4; i++) {
     if (answer === numGuess){
@@ -90,10 +90,10 @@ question6();
 
 
 function question7(){
-  var favSport = ['football', 'lacrosse', 'mma', 'soccer', 'boxing'];
+  let favSport = ['football', 'lacrosse', 'mma', 'soccer', 'boxing'];
 
-  for(var x = 0; x < 6; x++ ){
-    var bigGuess = prompt('Can you guess one of my favorite sports?');
+  for(let x = 0; x < 6; x++ ){
+    let bigGuess = prompt('Can you guess one of my favorite sports?');
     if (favSport.includes(bigGuess.toLowerCase)){
       alert('You are correct!');
       correct++;
