fe3ae0fa3bb9a5e6841f252775c0157a430b7ccf
mohammedalsamki <momosamak21@gmail.com>
2021-07-27 16:13:42 +0300

    editing my quastions and js code

--- js/app.js
+++ js/app.js
@@ -1,124 +1,109 @@
 'use strict';
 
-var score = 0;
-var name = prompt('What is your name?');
-console.log('User name: ' + name);
+function nameOfCustomer(){
+  var customerName = prompt('What is your name?');
+  alert('Hello ' + customerName + ' nice to meet you! Welcome to my site.');
+}
+nameOfCustomer();
 
-alert('Hi ' + name + ', Welcome to my page!');
-alert('let\'s play a guessing game about me. \nOnly Yes or No answers are valid.');
+var correct = 0;
 
-function yesCorrect (question) {
-    if (question.toUpperCase() === 'YES') {
-        score++;
-        alert("Yes!, you are right.");
-    }
-    else if (question.toUpperCase() === 'NO'){
-        alert("Sorry, wrong answer.");
-    }
-    else {
-        alert("This answer is not valid");
-    }
+function question1(){
+  var childhood = prompt('Did I grow up in Seattle?');
+  if(childhood.toLowerCase() === 'no'){
+    alert('You are correct');
+    correct++;
+  }else{
+    alert('You are incorrect');
+  }}
+
+question1();
+
+function question2(){
+  var job = prompt('Have I worked in tech before?');
+  if(job.toUpperCase() === 'NO'){
+    alert('You are correct');
+    correct++;
+  }else{
+    alert('You are incorrect');
+  }}
+
+question2();
+
+
+function question3(){
+  var age = prompt('Am I 26 years of age?');
+  if(age.toLowerCase() === 'yes'){
+    alert('You are correct');
+    correct++;
+  }else{
+    alert('You are incorrect');
+  }}
+
+question3();
+
+
+function question4(){
+  var school = prompt('Did I attend College?');
+  if(school.toLowerCase() === 'yes'){
+    alert('You are correct');
+    correct++;
+  }else{
+    alert('You are incorrect');
+  }}
+
+question4();
+
+
+
+function question5(){
+  var employer = prompt('Is my ideal employer Lockheed?');
+  if(employer.toUpperCase() === 'YES'){
+    alert('You are correct');
+    correct++;
+  }else{
+    alert('You are incorrect');
+  }}
+
+question5();
+
+
+function question6(){
+  var guess = prompt('Guess a number between 1 and 10');
+  var numGuess = Number(guess);
+  var answer = 8;
+
+  for (var i = 0; i < 4; i++) {
+    if (answer === numGuess){
+      alert('That is correct!');
+      correct++;
+      break;
+    }else if (numGuess < answer){
+      numGuess = parseInt(prompt('Too Low, guess again'));
+    }else
+      numGuess = parseInt(prompt('That is too high, guess again!'));
+  }
+  alert('The correct answer was 8!');
 }
 
-function noCorrect (question) {
-    if (question.toUpperCase() === 'YES') {
-        alert("Sorry, wrong answer.");
+question6();
+
+
+function question7(){
+  var favSport = ['football', 'lacrosse', 'mma', 'soccer', 'boxing'];
+
+  for(var x = 0; x < 6; x++ ){
+    var bigGuess = prompt('Can you guess one of my favorite sports?');
+    if (favSport.includes(bigGuess.toLowerCase)){
+      alert('You are correct!');
+      correct++;
+      break;
+    }else {
+      alert('Sorry, please try again');
     }
-    else if (question.toUpperCase() === 'NO'){
-        score++;
-        alert("Yes! you are right.");
-    }
-    else {
-        alert("This answer is not valid");
-    }
-}
+  }}
+
+question7();
 
 
-// Guess Question 1
-var favColor = prompt(name + " Is my favorite color Red? Yes or No:");
-console.log('my favorite color: ' + favColor);
-
-noCorrect(favColor);
-
-
-// Guess Question 2
-var coffee = prompt(name + ' Do I like coffee? Yes or No:');
-console.log('coffee: ' + coffee);
-
-noCorrect(coffee);
-
-
-// Guess Question 3
-var rollerCoaster = prompt(name + ' Do I like roller coasters? Yes or No');
-console.log('roller coaster: ' + rollerCoaster);
-
-noCorrect(rollerCoaster);
-
-// Guess Question 4
-var macUser = prompt(name + ' Do I prefer Mac computers? Yes or No');
-console.log('mac user: ' + macUser);
-
-yesCorrect(macUser);
-
-
-// Guess Question 5
-var windowsUser = prompt(name + ' Do I prefer Windows computers? Yes or No');
-console.log('windows user: ' + windowsUser);
-
-noCorrect(windowsUser);
-
-
-// Guess Question 6
-function guessFavNumber() {
-    var counter = 1;
-    if (counter === 5) {
-        alert('Sorry you run out of guesses');
-    }
-    while (counter < 5 && number !==33) {
-        var number = parseInt(prompt('Guess my favorite number'));
-        if (number > 33) {
-            alert('You guessed too high! Guess again');
-            counter++;
-        } else if (number < 33) {
-            alert('You guessed too low! Guess again');
-            counter++;
-        }
-    }
-    if (number === 33) {
-        alert('That is correct! My favorite number is 33!');
-        score++;
-        counter = 5;
-    }
-}
-
-guessFavNumber();
-
-
-// Guess Question 7
-
-function questionSeven() {
-    var userFavFruit = ['apple', 'strawberry', 'orange', 'grape', 'watermelon'];
-
-    for (var i = 0; i < 6; i++) {
-        var userAnsw = prompt('Can you guess my favorite fruit?');
-        var guess = userAnsw.toLowerCase();
-
-        for (var j = 0; j < userFavFruit.length; j++){
-            if (guess === userFavFruit[j]) {
-                alert('Congrats you got it right!!!');
-                score++;
-                i = 6;
-                break;
-            }
-        }
-        if (i !==6){
-            alert('Sorry wrong answer, try again');
-        }
-    }
-}
-
-questionSeven();
-
-alert('My favorite fruits are: apple, strawberry, orange, grape and watermelon');
-
-alert('Good Job!... You got ' + score + ' correct answers!');
+alert('You got ' + correct + '/7');
